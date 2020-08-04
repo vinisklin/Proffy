26f4d696d96bfb6358aa0397a68af3d725b1b1ed
vinisklin <vinisklin@gmail.com>
2020-08-04 12:37:24 -0300

    Create: SQLite3 DB and migrations to all tables

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json);
+
 app.get('/users', (request, response) => {
   const users = [
-    { "name": "Vinicius", "age": 25 },
-    { "name": "Andressa", "age": 21 },
+    { name: "Vinicius", age: 25 },
+    { name: "Andressa", age: 21 },
   ];
-  
+
   return response.json(users);
 });
 
