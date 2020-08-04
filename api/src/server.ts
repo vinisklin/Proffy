94cb7ec49fddefe818361352c54b639c549480f1
vinisklin <vinisklin@gmail.com>
2020-08-04 11:51:55 -0300

    Create: Node.js API environment

---
+++ api/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  const users = [
+    { "name": "Vinicius", "age": 25 },
+    { "name": "Andressa", "age": 21 },
+  ];
+  
+  return response.json(users);
+});
+
+app.listen(3333);
